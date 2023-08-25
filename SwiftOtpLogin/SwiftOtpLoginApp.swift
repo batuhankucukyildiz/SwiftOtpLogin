ff5199c28a35117a03c4f74a67dfe915d2ed0c0f
Batuhan <kucukyildizbatuhan@gmail.com>
2023-08-25 23:54:58 +0300

    Initial Commit

---
+++ SwiftOtpLogin/SwiftOtpLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftOtpLoginApp.swift
+//  SwiftOtpLogin
+//
+//  Created by Batuhan Küçükyıldız on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftOtpLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
